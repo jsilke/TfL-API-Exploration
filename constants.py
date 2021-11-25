79843b5702b3f3dd529239820694775c63a1dc2d
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2021-11-25 18:54:08 -0500

    Replace hand coded strings with constants. Move constants from main.py to constants.py file.

---
+++ constants.py
@@ -0,0 +1,21 @@
+import os
+
+# ------------------------------------------API-specific--------------------------------------------
+
+# Retrieve API keys from environment variables.
+PRIMARY_KEY = os.getenv('TfL_pkey')
+SECONDARY_KEY = os.getenv('TfL_skey')
+AIR_QUALITY_URL = "https://api.tfl.gov.uk/AirQuality/"
+VALID_MODES_URL = "https://api.tfl.gov.uk/Line/Meta/Modes/"
+
+# ----------------------------------------Local Pathing---------------------------------------------
+
+DIRECTORY = './data/'
+MOST_RECENT = 'most_recent.json'
+AIR_QUALITY_FILE = 'AirQuality.json'
+LINE_FILE = 'Line.json'
+
+# ---------------------------------------Reusable Strings-------------------------------------------
+
+JSON_PARSE_HINT = 'Expected JSON-formatted response from the TfL API.'
+FILE_NOT_FOUND = 'FileNotFoundError - No such file or directory:'  # Custom message
--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+from constants import *
 import os
 import json
 import requests as re
 
 
-# Retrieve API keys from environment variables.
-PRIMARY_KEY = os.getenv('TfL_pkey')
-SECONDARY_KEY = os.getenv('TfL_skey')
-
-
-def get_json(url: str = "https://api.tfl.gov.uk/AirQuality/",
-             authentication: tuple = (PRIMARY_KEY, SECONDARY_KEY)) -> re.Response:
+def get_json(url: str, authentication: tuple = (PRIMARY_KEY, SECONDARY_KEY)) -> re.Response:
     """Query the LtF AirQuality API"""
     response = re.get(url, auth=authentication)
     if response.status_code == 200:
@@ -17,12 +12,12 @@ def get_json(url: str = "https://api.tfl.gov.uk/AirQuality/",
     print(f'API returned:{response.status_code}')
 
 
-def store_response_json(json_object: dict, file_name='AirQuality.json',
-                        directory: str = './data/') -> None:
+def store_response_json(json_object: dict, file_name: str = MOST_RECENT,
+                        directory: str = DIRECTORY) -> None:
     """
     Store json in file.
     """
-    _path = directory + file_name
+    _path = directory + file_name  # Concatenate the strings.
     if not os.path.exists(directory):
         os.mkdir(directory)
 
@@ -30,15 +25,16 @@ def store_response_json(json_object: dict, file_name='AirQuality.json',
         json.dump(json_object, json_file, sort_keys=True, indent=4)
 
 
-def json_from_file(path: str = './data/AirQuality.json') -> dict:
+def json_from_file(file_name: str, directory: str = DIRECTORY) -> dict:
     """
     Retrieve json from file.
     """
+    _path = directory + file_name  # Concatenate the strings.
     try:
-        with open(path) as json_file:
+        with open(_path) as json_file:
             query_dict = json.load(json_file)
     except FileNotFoundError:
-        print(f'FileNotFoundError - No such file or directory: {path}')
+        print(f'{FILE_NOT_FOUND} {_path}')
 
     return query_dict
 
@@ -48,13 +44,16 @@ def tomorrow_forecast(json_object: dict) -> str:
     """Returns tomorrow's forcast."""
     try:
         _tomorrow_forecast_dict = json_object['currentForecast'][1]
-        # Explicit line continuation below.
+        # Explicit line continuation.
         _tomorrow_forecast_str = f"{_tomorrow_forecast_dict['forecastSummary']}\n\
                                    {_tomorrow_forecast_dict['forecastText']}"
     except KeyError:
-        print(f'{KeyError}: Expected JSON-formatted response from the TfL API.')
+        print(f'{KeyError}: {JSON_PARSE_HINT}')
 
     return _tomorrow_forecast_str
+
+
+# -------------------------------------------------TASK 2-----------------------------------------------
 
 
 def main():
