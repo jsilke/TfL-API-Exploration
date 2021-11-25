c1422de1b9ad99513122a5bac46d816afc4d2817
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2021-11-25 12:54:03 -0500

    Add function to retrieve json data from a file. Modify functions to incorporate json module features.

--- main.py
+++ main.py
@@ -11,13 +11,29 @@ url_keys_append = f'?app_id={PRIMARY_KEY}&app_key={SECONDARY_KEY}'
 url = "https://api.tfl.gov.uk/AirQuality"
 
 
-def store_request_json(json: dict, path: str = './data/AirQuality.json') -> None:
+def store_response_json(json_object: dict, path: str = './data/AirQuality.json') -> None:
+    """
+    Store json in file.
+    """
     with open(path, 'a') as json_file:
-        json_file.write(json)
+        json.dump(json_object, json_file)
+
+
+def dict_from_json(path: str = './data/AirQuality.json') -> dict:
+    """
+    Retrieve json from file.
+    """
+    try:
+        with open(path) as json_file:
+            query_dict = json.load(json_file)
+    except FileNotFoundError:
+        print(f'FileNotFoundError - No such file or directory: {path}')
+
+    return query_dict
 
 
 def main():
-    pass
+    dict_from_json()
 
 
 if __name__ == '__main__':
