3206fabc0bb4aff38a53308a45c54612a5c98ca1
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2021-11-25 16:35:50 -0500

    Separate the core project code into the .py file and the exercises into the .ipynb

--- main.py
+++ main.py
@@ -2,24 +2,36 @@ import os
 import json
 import requests as re
 
+
 # Retrieve API keys from environment variables.
 PRIMARY_KEY = os.getenv('TfL_pkey')
 SECONDARY_KEY = os.getenv('TfL_skey')
 
-# Remove from global namespace later.
-url_keys_append = f'?app_id={PRIMARY_KEY}&app_key={SECONDARY_KEY}'
-url = "https://api.tfl.gov.uk/AirQuality"
+
+def get_json(url: str = "https://api.tfl.gov.uk/AirQuality/",
+             authentication: tuple = (PRIMARY_KEY, SECONDARY_KEY)) -> re.Response:
+    """Query the LtF AirQuality API"""
+    response = re.get(url, auth=authentication)
+    if response.status_code == 200:
+        return response.json()
+    print(f'API returned:{response.status_code}')
 
 
-def store_response_json(json_object: dict, path: str = './data/AirQuality.json') -> None:
+def store_response_json(json_object: dict, file_name='AirQuality.json',
+                        directory: str = './data/') -> None:
     """
     Store json in file.
     """
-    with open(path, 'a') as json_file:
-        json.dump(json_object, json_file)
+    _path = directory + file_name
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+
+    with open(_path, 'w') as json_file:
+        json.dump(json_object, json_file, sort_keys=True, indent=4)
 
 
-def dict_from_json(path: str = './data/AirQuality.json') -> dict:
+def json_from_file(path: str = './data/AirQuality.json') -> dict:
+    # TODO: test this function!
     """
     Retrieve json from file.
     """
@@ -33,7 +45,9 @@ def dict_from_json(path: str = './data/AirQuality.json') -> dict:
 
 
 def main():
-    dict_from_json()
+    json_object = get_json()
+    store_response_json(json_object)
+    print('Done!')
 
 
 if __name__ == '__main__':
