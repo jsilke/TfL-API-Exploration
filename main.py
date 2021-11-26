197f841b05323420be891666a4c96bca82e775af
Jordan Silke <51223540+jsilke@users.noreply.github.com>
2021-11-26 12:45:48 -0500

    Modify existing functions to generalize better. Add functions for task 7.

--- main.py
+++ main.py
@@ -48,27 +48,28 @@ def tomorrow_forecast(json_object: dict) -> str:
     """Returns tomorrow's forcast."""
     try:
         _tomorrow_forecast_dict = json_object['currentForecast'][1]
-        _tomorrow_forecast_str = f"{_tomorrow_forecast_dict['forecastSummary']}\n\
-                                   {_tomorrow_forecast_dict['forecastText']}"  # ^ Explicit line continuation. 🤡
+        _tomorrow_forecast_str = f"{_tomorrow_forecast_dict['forecastSummary']}\n{_tomorrow_forecast_dict['forecastText']}"
     except KeyError:
         print(f'{KeyError}: {JSON_PARSE_HINT}')
 
     return _tomorrow_forecast_str
 
 
-def valid_tfl_transportation(json_object: list) -> tuple[list, int]:
+def valid_tfl_transportation(url: str = VALID_MODES_URL) -> tuple[list, int]:
     """
     Takes in response from the GET request to https://api.tfl.gov.uk/Line/Meta/Modes/
     and returns a tuple containing the list of TfL modes of transportation as well
     as the number of elements in the list.
     """
+    json_object = get_json(url)
     valid_modes_list = [transportation_mode['modeName']
                         for transportation_mode in json_object
                         if transportation_mode['isTflService']]
     # Make a nicely presentable string.
-    valid_modes_string = '\n- '.join(valid_modes_list)
+    tfl_modes = '\n- '.join(valid_modes_list)
 
-    return (valid_modes_string, len(valid_modes_list))
+    print(
+        f'TfL\'s modes of transportation include:\n{tfl_modes}\nThus, TfL offers {len(valid_modes_list)} forms of transportation at present.')
 
 
 def get_valid_lines(url: str = ALL_MODES, transportation_modes: str = 'bus, tube') -> re.Response:
@@ -116,9 +117,36 @@ def count_line_stations(json_object: list) -> int:
     return _station_sum
 
 
+def get_stop_points(origin: str = "Heathrow Airport", destination: str = "Tower Bridge", url: str = STOP_POINTS) -> tuple[str, str]:
+    """Get stop point coordinates for origin and destination and return them as a tuple."""
+    # TODO Get coordinates for a station that isn't out of service!
+    origin, destination = get_json(url, parameters={'query': origin}),\
+        get_json(url, parameters={'query': destination})
+
+    # Get the latitude and longitude as comma separated strings for origin and destination.
+    origin_lat_lon, destination_lat_lon = f"{origin['matches'][0]['lat'], origin['matches'][0]['lon']}",\
+                                          f"{destination['matches'][0]['lat'], destination['matches'][0]['lon']}"
+
+    return (origin_lat_lon.strip('()'), destination_lat_lon.strip('()'))
+
+
+def plan_journey(from_and_to: tuple[str, str], mode: str, url: str = JOURNEY_RESULTS):
+    """Currently only works for bus and tube, can be generalized easily."""
+    _JOURNEY_URL = f"{url}{from_and_to[0]}/to/{from_and_to[1]}"
+
+    trip = get_json(_JOURNEY_URL, parameters={'mode': mode})
+    store_response_json(trip)
+    trip_duration = trip['journeys'][0]['duration']
+
+    # Location needs to be generalized.
+    return f'A {mode} trip from Heathrow Airport to Tower bridge will take {trip_duration} minutes.'
+
+# TODO Add functions for task 4!
+
+
 def main():
-    json_object = get_line_stations()
-    print(count_line_stations(json_object))
+    # print(plan_journey(get_stop_points(), 'bus'))
+    print(plan_journey(get_stop_points(), 'bus'))
 
 
 if __name__ == '__main__':
